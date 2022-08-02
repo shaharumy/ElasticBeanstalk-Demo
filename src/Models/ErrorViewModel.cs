7bd757dbbf1e820ede62fe2a925bdf2abe96f327
Danilo Quilici <daniloquilici@gmail.com>
2022-08-02 16:19:22 -0300

    Add simple project .NET Core 6 MVC

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElasticBeanstalk_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
